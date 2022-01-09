3701c706ce5cb67f5986992f1a2a3b85665848ea
E.M.C.P. Ekanayake <chamara.sab@outlook.com>
2022-01-09 15:32:17 +0530

    completed

--- loops/js/main.js
+++ loops/js/main.js
@@ -1,7 +1,7 @@
 function array(end) {
-    var numbers = new Array(100);                                                   //define array of 100 integers
+    var numbers = new Array(end);                                                   //define array of 100 integers
     let sum = 0;
-    for (let i = 0; i < numbers.length; i++) {
+    for (let i = 0; i < end; i++) {
         numbers[i] = i + 1;                                                         //initilzing the array 1 - 100
         sum += i + 1;                                                               //calculating total each
     }
