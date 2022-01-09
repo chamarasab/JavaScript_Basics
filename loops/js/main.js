2c96ecb32cc79814eed1f3ac708ee65b449988d1
E.M.C.P. Ekanayake <chamara.sab@outlook.com>
2022-01-09 15:20:10 +0530

    commentings

--- loops/js/main.js
+++ loops/js/main.js
@@ -1,14 +1,14 @@
 function array(end) {
-    var numbers = new Array(100);       //define array of 100 integers
+    var numbers = new Array(100);                                                   //define array of 100 integers
     let sum = 0;
     for (let i = 0; i < numbers.length; i++) {
-        numbers[i] = i + 1;             //initilzing the array 1 - 100
-        sum += i + 1;                   //calculating total each
+        numbers[i] = i + 1;                                                         //initilzing the array 1 - 100
+        sum += i + 1;                                                               //calculating total each
     }
     
     for (let i = 0; i < numbers.length; i++) {
-        document.getElementById('txtOutput').innerHTML += numbers[i] + " ";             //printing array elements
+        document.getElementById('txtOutput').innerHTML += numbers[i] + " ";         //printing array elements
     }
 
-    document.getElementById('txtTotal').innerHTML = sum;        //printing total amount
+    document.getElementById('txtTotal').innerHTML = sum;                            //printing total amount
 }
