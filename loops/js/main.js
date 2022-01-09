aa6f52d5f2ae1af0f5beb17fa2e1672a094bf200
E.M.C.P. Ekanayake <chamara.sab@outlook.com>
2022-01-09 16:20:18 +0530

    Completed

--- loops/js/main.js
+++ loops/js/main.js
@@ -1,6 +1,7 @@
 function array(end) {
     var numbers = new Array(end);                                                   //define array of 100 integers
     let sum = 0;
+    let evenTotal = 0;
     
     for (let i = 0; i < end; i++) {
         numbers[i] = i + 1;                                                         //initilzing the array 1 - 100
@@ -11,5 +12,14 @@ function array(end) {
         document.getElementById('txtOutput').innerHTML += numbers[i] + " ";         //printing array elements
     }
 
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 == 0) {
+            document.getElementById('txtEvenOutput').innerHTML += numbers[i] + " ";
+            evenTotal += numbers[i];
+        }
+        
+    }
+
     document.getElementById('txtTotal').innerHTML = sum;                            //printing total amount
+    document.getElementById('txtEvenTotal').innerHTML = evenTotal;                  //printing total amount only even numbers
 }
